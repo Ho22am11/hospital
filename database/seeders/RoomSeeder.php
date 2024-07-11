82193f2c3ba35d693c3ff1228d3caa59951f21a6
Hossam harraz <harrazhossam11@gmail.com>
2024-07-11 22:29:39 +0300

    make seeder rooms

---
+++ database/seeders/RoomSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Room;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RoomSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+       
+        for ($num_room = 1 ; $num_room <= 100 ; $num_room++ ){
+            Room::create(['name' => $num_room , 'status' => false ]);
+        } 
+    }
+}
